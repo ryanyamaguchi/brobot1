2a8145e962a1b1d58eec31bd04877d61ae9e4a6c
ryanyamaguchi <39916693+ryanyamaguchi@users.noreply.github.com>
2018-06-04 18:24:11 +0300

    Add files via upload

---
+++ Hello_World (1).py
@@ -0,0 +1,14 @@
+
+x = 0
+
+print("starting hacking")
+while x < 1000:
+    print("3r28478236487v3qyg i8asfyddstfv w78eryvweiuavvhr3w875y")
+    print("adyaskudhkasuhdkasihfkausdfhakuuhfkuashdfkuashdkasudasdas")
+    print("ronisasalutronisasalutronisasalutronisasalutronisasalut")
+    print("4239487312947329847329842394723984")
+    x = x + 1
+    
+    
+print("Hello World")
+print("Who ever has just run this their IP has just been sent to Luke")
